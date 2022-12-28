68fd55a70a1f8d16ba091a0b7a64f8ce93d0b609
oneku16 <eku.ulanov@yandex.com>
2022-12-28 21:49:10 +0600

    small progress

--- src/main/java/com/example/calculator_2/Application.java
+++ src/main/java/com/example/calculator_2/Application.java
@@ -1,23 +1,22 @@
 package com.example.calculator_2;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("home_page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 420);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         launch();
     }
 }
---
+++ src/main/java/com/example/calculator_2/ApplicationController.java
@@ -0,0 +1,35 @@
+package com.example.calculator_2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+public class ApplicationController {
+
+    @FXML
+    private GridPane allButtons;
+
+    @FXML
+    private Button findButton;
+
+    @FXML
+    private Button findButton1;
+
+    @FXML
+    private VBox getStudentData;
+
+    @FXML
+    private VBox getStudentData1;
+
+    @FXML
+    private VBox getStudentData11;
+
+    @FXML
+    private TextField inputFirstName;
+
+    @FXML
+    private TextField inputLastName;
+
+}
